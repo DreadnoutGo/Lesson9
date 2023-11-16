aae4af605bf93ca01d68a85bf41a0f5f8196453c
Dreadnout <rfiflaa@gmail.com>
2023-11-16 21:48:51 +0300

    task2

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿int M1 = 1, N1 = 5;
+Console.WriteLine($"M = {M1}; N = {N1} -> \"{PrintEvenNumbers(M1, N1)}\"");
+
+int M2 = 4, N2 = 8;
+Console.WriteLine($"M = {M2}; N = {N2} -> \"{PrintEvenNumbers(M2, N2)}\"");
+
+static string PrintEvenNumbers(int m, int n)
+{
+    if (m > n)
+    {
+        return ""; // если m больше n, возвращаем пустую строку
+    }
+
+    // Рекурсивный случай: если m четное добавляем его к результату и вызываем функцию для следующего четного числа
+    if (m % 2 == 0)
+    {
+        return m + (m < n - 1 ? ", " : "") + PrintEvenNumbers(m + 2, n);
+    }
+    // Если m нечетное вызываем функцию для следующего числа
+    else
+    {
+        return PrintEvenNumbers(m + 1, n);
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿int M1 = 1, N1 = 15;
+Console.WriteLine($"M = {M1}; N = {N1} -> {CalculateSum(M1, N1)}");
+
+int M2 = 4, N2 = 8;
+Console.WriteLine($"M = {M2}; N = {N2} -> {CalculateSum(M2, N2)}");
+
+
+static int CalculateSum(int m, int n)
+{
+    if (m > n)
+    {
+        return 0; //если m больше n, возвращаем 0
+    }
+
+    //возвращаем сумму текущего числа и суммы для следующего числа
+    return m + CalculateSum(m + 1, n);
+}
